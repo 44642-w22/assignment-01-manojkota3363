d74191efc9e8f0b056c3df60f4fa373f464182f8
Manoj Kota <kota.manoj8@gmail.com>
2022-02-07 21:27:46 -0600

    Update AssignmentMain.java

--- src/assignment1/AssignmentMain.java
+++ src/assignment1/AssignmentMain.java
@@ -25,7 +25,7 @@ for(int i=0;i<n;i++) {
 
 AssignmentTest1 Obj1 = new AssignmentTest1();
 		L1 = Obj1.problem01(L1);
-		System.out.println(L1);
+		System.out.println("Output (L2):"+L1);
 		System.out.println("");
 		
 		System.out.println("******problem2******");
@@ -41,7 +41,7 @@ AssignmentTest1 Obj1 = new AssignmentTest1();
 		}
 		System.out.println("enter the  number that array addition must be equal ");
 int y=sc.nextInt();
-		System.out.println(Obj1.problem2(L2, y));
+		System.out.println("Output (L2): "+Obj1.problem2(L2, y));
 		
 	
 		System.out.println("******problem3******");
@@ -54,7 +54,7 @@ for(int i=0;i<n2;i++) {
 	L3.add(sc.nextInt());
 }
 
-		System.out.println(Obj1.problem3(L3));
+		System.out.println("Output:"+Obj1.problem3(L3));
 	
 		System.out.println("enter size of arraylist");
 
@@ -68,21 +68,22 @@ for(int i=0;i<n2;i++) {
 			L4.add(sc.nextLine());
 		}
 
-		System.out.println(Obj1.problem4(L4));
+		System.out.println("Output (A2): "+Obj1.problem4(L4));
 		System.out.println("enter the String pattern");
 
 		String s=sc.nextLine();	
 		System.out.println("******problem5******");
 
-		System.out.println(Obj1.problem05(s));
+		System.out.println("Output: "+Obj1.problem05(s));
 		
 		
 		Stack<Integer> S1 = new Stack<>();
 				System.out.println("******problem6******");
 		System.out.println("enter the Size of stack");
-		System.out.println("enter the elements of stack");
 
 		int n5=sc.nextInt();
+		System.out.println("enter the elements of stack");
+
 		for(int i=0;i<n5;i++) {
 			S1.push(sc.nextInt());
 		}
@@ -90,7 +91,7 @@ for(int i=0;i<n2;i++) {
 
 
 
-		System.out.println(Obj1.problem6(S1));
+		System.out.println("Output (A): "+Obj1.problem6(S1));
 	
 		
 		System.out.println("******problem7******");
@@ -106,7 +107,7 @@ for(int i=0;i<n2;i++) {
 			Q1.add(sc.nextLine());
 		}
 
-		System.out.println(Obj1.problem7(Q1));
+		System.out.println("Output: "+Obj1.problem7(Q1));
 		
 		
 		System.out.println("******problem8******");
@@ -122,12 +123,15 @@ for(int i=0;i<n2;i++) {
 			Q2.add(sc.nextInt());
 		}
 
-		System.out.println(Obj1.problem8(Q2));
+		System.out.println("Output (A): "+Obj1.problem8(Q2));
 
 		
 		
 		Queue<Integer> Q3 = new LinkedList<>();
-		
+		/*Q3.offer(5);
+		Q3.offer(3);
+		Q3.offer(1);
+		Q3.offer(4);*/
 		System.out.println("******problem9******");
 		System.out.println("enter the Size of Deque");
 
@@ -136,7 +140,7 @@ for(int i=0;i<n2;i++) {
 		for(int i=0;i<n8;i++) {
 			Q3.add(sc.nextInt());
 		}
-		System.out.println(Obj1.problem9(Q3));
+		System.out.println("Output (A): "+Obj1.problem9(Q3));
 		
 		
 		
@@ -159,10 +163,10 @@ for(int i=0;i<n2;i++) {
 		for(int i=0;i<n10;i++) {
 			arr[i]=sc.nextInt();
 		}
-		
-		System.out.println(Obj1.problem10(Q4,arr));
+		//Integer[] arr = {1, 1, 0};
+		System.out.println("Output :"+Obj1.problem10(Q4,arr));
 	
-	
+	sc.close();
 
 	}
 
